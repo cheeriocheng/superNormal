40c61e8720b9b07df96163e680e48c44e26a8f0d
Cheng <cheeriocheng@gmail.com>
2018-06-08 23:53:41 -0700

    draw triangle

---
+++ test.py
@@ -0,0 +1,49 @@
+import glfw
+import OpenGL.GL as gl
+
+
+def main():
+    # Initialize the library
+    if not glfw.init():
+        return
+    # Create a windowed mode window and its OpenGL context
+    window = glfw.create_window(640, 480, "Hello World", None, None)
+    if not window:
+        glfw.terminate()
+        return
+
+    # Make the window's context current
+    glfw.make_context_current(window)
+
+    # Loop until the user closes the window
+    while not glfw.window_should_close(window):
+        # Render here, e.g. using pyOpenGL
+        width, height = glfw.get_framebuffer_size(window)
+        ratio = width / float(height)
+        gl.glViewport(0, 0, width, height)
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT)
+        gl.glMatrixMode(gl.GL_PROJECTION)
+        gl.glLoadIdentity()
+        gl.glOrtho(-ratio, ratio, -1, 1, 1, -1)
+        gl.glMatrixMode(gl.GL_MODELVIEW)
+        gl.glLoadIdentity()
+        # gl.glRotatef(glfw.glfwGetTime() * 50, 0, 0, 1)
+        gl.glBegin(gl.GL_TRIANGLES)
+        gl.glColor3f(1, 0, 0)
+        gl.glVertex3f(-0.6, -0.4, 0)
+        gl.glColor3f(0, 1, 0)
+        gl.glVertex3f(0.6, -0.4, 0)
+        gl.glColor3f(0, 0, 1)
+        gl.glVertex3f(0, 0.6, 0)
+        gl.glEnd()
+
+        # Swap front and back buffers
+        glfw.swap_buffers(window)
+
+        # Poll for and process events
+        glfw.poll_events()
+
+    glfw.terminate()
+
+if __name__ == "__main__":
+    main()
