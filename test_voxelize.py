abf0f19d8100a1307d34649b448f8ce399eac9dd
Cheng <cheeriocheng@gmail.com>
2018-06-23 15:44:58 -0700

    tweaked parameters

--- test_voxelize.py
+++ test_voxelize.py
@@ -1,6 +1,6 @@
 from voxlib.voxelize import voxelize
 
 
-voxels = [v for v in voxelize("model/chair.obj", 30)]
+voxels = [v for v in voxelize("model/chair.obj", 60)]
 print(len(voxels))
 print(voxels)
